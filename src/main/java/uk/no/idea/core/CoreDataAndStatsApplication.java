b3513fa7390784a14a95654269407feb7a38b5d9
michal gebarowski <vmaaik@gmail.com>
2020-07-27 12:12:50 +0100

    init

---
+++ src/main/java/uk/no/idea/core/CoreDataAndStatsApplication.java
@@ -0,0 +1,13 @@
+package uk.no.idea.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreDataAndStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreDataAndStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/no/idea/core/CoreDataAndStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.no.idea.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreDataAndStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
